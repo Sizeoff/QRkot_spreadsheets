af2d8b764c9b44dbfdce50d7291111fda76b948b
unknown <vladsizov96@yandex.ru>
2023-03-30 22:35:15 +0300

    hw_checked

--- app/api/endpoints/charity_project.py
+++ app/api/endpoints/charity_project.py
@@ -4,10 +4,10 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.validators import (check_charity_project_already_invested,
-                            check_charity_project_closed,
-                            check_charity_project_exists,
-                            check_charity_project_invested_sum,
-                            check_name_duplicate)
+                                check_charity_project_closed,
+                                check_charity_project_exists,
+                                check_charity_project_invested_sum,
+                                check_name_duplicate)
 from app.core.db import get_async_session
 from app.core.user import current_superuser
 from app.crud.charity_project import charity_project_crud
@@ -27,12 +27,10 @@ router = APIRouter()
     dependencies=[Depends(current_superuser)],
 )
 async def create_charity_project(
-    charity_project: CharityProjectCreate,
-    session: AsyncSession = Depends(get_async_session),
+        charity_project: CharityProjectCreate,
+        session: AsyncSession = Depends(get_async_session),
 ):
-    """Только для суперюзеров.
-    Создает благотворительный проект.
-    """
+    """Только для суперюзеров"""
     await check_name_duplicate(charity_project.name, session)
     await charity_project_crud.get_project_id_by_name(
         charity_project.name, session
@@ -48,7 +46,7 @@ async def create_charity_project(
     response_model_exclude_none=True,
 )
 async def get_all_charity_projects(
-    session: AsyncSession = Depends(get_async_session),
+        session: AsyncSession = Depends(get_async_session),
 ):
     """Получает список всех проектов."""
     all_projects = await charity_project_crud.get_multi(session)
@@ -61,9 +59,9 @@ async def get_all_charity_projects(
     dependencies=[Depends(current_superuser)],
 )
 async def update_charity_project(
-    project_id: int,
-    obj_in: CharityProjectUpdate,
-    session: AsyncSession = Depends(get_async_session),
+        project_id: int,
+        obj_in: CharityProjectUpdate,
+        session: AsyncSession = Depends(get_async_session),
 ):
     """Только для суперюзеров.
     Закрытый проект нельзя редактировать,
@@ -90,8 +88,8 @@ async def update_charity_project(
     dependencies=[Depends(current_superuser)],
 )
 async def delete_charity_project(
-    project_id: int,
-    session: AsyncSession = Depends(get_async_session),
+        project_id: int,
+        session: AsyncSession = Depends(get_async_session),
 ):
     """Только для суперюзеров.
     Удаляет проект. Нельзя удалить проект,
